8df5266c8441131a22c5492dcd55dfaf6364902b
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2025-05-08 20:57:38 +0900

    jobs, fg, spawn_childの実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
-mod helper;
+// mod helper;
 mod shell;
 
+/// コマンドをパース
+fn parse_cmd(line: &str) -> () {
+    let commands: Vec<&str> = line.split('|').map(|x| x.trim()).collect();
+    let mut result = vec![];
+
+    for cmd in commands {
+        match cmd {
+            "" => panic!("Invalid arguments"),
+            _ => {
+                let command: Vec<&str> = cmd.split(" ").collect();
+                if let Some(&c) = command.get(0) {
+                    if let Some(_) = command.get(1) {
+                        let mut args = vec![];
+                        for i in 1..command.len() {
+                            let arg = command[i];
+                            args.push(arg);
+                        }
+                        result.push((c, args))
+                    } else {
+                        result.push((c, vec![]));
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", result);
+}
+
 fn main() {
+    // let line = "echo hello | |  less";
+    let line = "echo hello | less | cat";
+    parse_cmd(line);
+
     use nix::libc;
     println!("{}", libc::STDIN_FILENO);
 }
--- src/shell.rs
+++ src/shell.rs
@@ -189,7 +189,7 @@ impl Worker {
                         match parse_cmd(&line) {
                             // コマンド実行メッセージの場合、parse_cmdでメッセージをパース。
                             Ok(cmd) => {
-                                if self.build_in_cmd(&cmd, &shell_tx) {
+                                if self.built_in_cmd(&cmd, &shell_tx) {
                                     // 組み込みコマンド（シェルの内部コマンド）を実行。
                                     // 組み込みコマンドならworker_rxから受信
                                     continue;
@@ -215,5 +215,205 @@ impl Worker {
                 }
             }
         });
+    }
+
+    fn built_in_cmd(&mut self, cmd: &[(&str, Vec<&str>)], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        if cmd.len() > 1 {
+            return false; // 組み込みコマンドのパイプは非対応なのでエラー, 最初のコマンドのみ実行
+        }
+
+        match cmd[0].0 {
+            "exit" => self.run_exit(&cmd[0].1, shell_tx),
+            // "jobs" => self.run_jobs(shell_tx),
+            "fg" => self.run_fg(&cmd[0].1, shell_tx),
+            // "cd" => self.run_cd(&cmd[0].1, shell_tx),
+            _ => false,
+        }
+    }
+
+    /// 子プロセスを生成。失敗した場合はシェルからの入力を再開させる必要あり。
+    fn spawn_child(&mut self, line: &str, cmd: &[(&str, Vec<&str>)]) -> bool {
+        assert_ne!(cmd.len(), 0); // コマンドが空でないか検査
+
+        // ジョブIDを取得
+        let job_id = if let Some(id) = self.get_new_job_id() {
+            id
+        } else {
+            eprintln!("ZeroSh: 管理可能なジョブを最大値に到達。");
+            return false;
+        };
+
+        if cmd.len() > 2 {
+            eprintln!("ZeroSh: 3つ以上のコマンドによるパイプはサポートしていません");
+            return false;
+        }
+
+        let mut input = None; // 2つ目のプロセスの標準入力
+        let mut output = None; // 1つ目のプロセスの標準出力
+        if cmd.len() == 2 {
+            // パイプを作成
+            let p = pipe().unwrap();
+            input = Some(p.0);
+            output = Some(p.1);
+        }
+
+        // パイプを閉じる関数を定義
+        let cleanup_pipe = CleanUp {
+            f: || {
+                if let Some(fd) = input {
+                    syscall(|| unistd::close(fd)).unwrap();
+                }
+
+                if let Some(fd) = output {
+                    syscall(|| unistd::close(fd)).unwrap();
+                }
+            },
+        };
+
+        let pgid;
+        // 1つ目のプロセスを生成
+        match fork_exec(Pid::from_raw(0), cmd[0].0, &cmd[0].1, None, output) {
+            Ok(child) => pgid = child,
+            Err(e) => {
+                eprintln!("ZeroSh: プロセス生成エラー: {}", e);
+                return false;
+            }
+        }
+
+        // プロセス、ジョブの情報を追加
+        let info = ProcInfo {
+            state: ProcState::Run,
+            pgid,
+        };
+        let mut pids = HashMap::new();
+        pids.insert(pgid, info.clone()); // 1つ目のプロセスの取得
+
+        // 2つ目のプロセスのを生成
+        if cmd.len() == 2 {
+            match fork_exec(pgid, cmd[1].0, &cmd[1].1, input, None) {
+                Ok(child) => {
+                    pids.insert(child, info);
+                },
+                Err(e) => {
+                    eprintln!("ZeroSh: プロセス生成エラー: {}", e),
+                    return false;
+                }
+            }
+        }
+
+        std::mem::drop(cleanup_pipe); // パイプをクローズ
+
+        // ジョブ情報を追加して子プロセスをフォアグラウンドプロセスグループにする
+        self.fg = Some(pgid);
+        self.insert_job(job_id, pgid, pids, line);
+        tcsetpgrp(libc::STDIN_FILENO, pgid).unwrap();
+
+        true
+    }
+
+    /// exitコマンドを実行
+    fn run_exit(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        // 実行中のジョブがある場合は終了しない
+        if !self.jobs.is_empty() {
+            eprintln!("ジョブが実行中なので終了できません。");
+            self.exit_val = 1; // 失敗
+            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap(); // シェルを再開
+            return true;
+        }
+
+        // 終了コードを取得
+        let exit_val = if let Some(s) = args.get(1) {
+            if let Ok(n) = (*s).parse::<i32>() {
+                n
+            } else {
+                // 終了コードが整数ではない（i32のparseに失敗)
+                eprintln!("{}は不正な引数です。", s);
+                self.exit_val = 1;
+                shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap(); // シェルを再開
+                return true;
+            }
+        } else {
+            self.exit_val
+        };
+
+        shell_tx.send(ShellMsg::Quit(exit_val)).unwrap();
+        true
+    }
+
+    /// fgコマンドを実行。
+    fn run_fg(&mut self, args: &[&str], shell_tx: &SyncSender<ShellMsg>) -> bool {
+        self.exit_val = 1; // とりあえず失敗に設定
+
+        // 引数をチェック
+        if args.len() < 2 {
+            eprintln!("usage: fg <num>");
+            shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap(); // シェルを再開
+            return false;
+        }
+
+        // ジョブIDを取得
+        if let Ok(n) = args[1].parse::<usize>() {
+            if let Some((pgid, cmd)) = self.jobs.get(&n) {
+                eprintln!("[{}] 再開\t{}", n, cmd);
+
+                // フォアグラウンドプロセスに設定
+                self.fg = Some(*pgid);
+                tcsetpgrp(libc::STDIN_FILENO, *pgid).unwrap();
+
+                // ジョブの実行を再開
+                killpg(*pgid, Signal::SIGCONT).unwrap();
+                return true;
+            }
+        };
+
+        // 失敗
+        eprintln!("{}というジョブは見つかりませんでした。", args[1]);
+        shell_tx.send(ShellMsg::Continue(self.exit_val)).unwrap(); // シェルを再開
+        true
+    }
+}
+
+type CmdResult<'a> = Result<Vec<(&'a str, Vec<&'a str>)>, DynError>;
+
+/// コマンドをパース
+fn parse_cmd(line: &str) -> CmdResult {
+    let commands: Vec<&str> = line.split('|').map(|x| x.trim()).collect();
+    let mut result = vec![];
+
+    for cmd in commands {
+        let command: Vec<&str> = cmd.split(" ").collect();
+        if let Some(&c) = command.get(0) {
+            if let Some(_) = command.get(1) {
+                let mut args = vec![];
+                for i in 1..command.len() {
+                    let arg = command[i];
+                    println!("{}", arg);
+                    args.push(arg);
+                }
+                result.push((c, args))
+            } else {
+                result.push((c, vec![]));
+            }
+        }
+    }
+
+    Ok(result)
+}
+
+/// ドロップ時にクロージャFを呼び出す型。
+struct CleanUp<F>
+where
+    F: Fn(),
+{
+    f: F,
+}
+
+impl<F> Drop for CleanUp<F>
+where
+    F: Fn(),
+{
+    fn drop(&mut self) {
+        // クロージャをドロップ時に実行v
+        (self.f)()
     }
 }
